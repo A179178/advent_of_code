14b37522a5e38873a49465fdc1641d733a762f1c
Xuan Chen <xuan_chen@progressive.com>
2022-12-09 09:33:41 -0500

    Modify day 1 solution

--- 2022/day_01/Day_01_Solution.py
+++ 2022/day_01/Day_01_Solution.py
@@ -1,35 +1,17 @@
 
 # coding: utf-8
-
-# In[7]:
-
-
 ## Day 1 Solution
-
-
-# In[12]:
-
 
 ### Import packages
 import pandas as pd
 import numpy as np
 
 
-# In[255]:
-
-
 ### Import Input file 
 with open ('U:\Test\Input.txt', 'r') as input_file:
         input_raw = input_file.read().splitlines()
 
-
-# In[256]:
-
-
 input_raw[0:20]
-
-
-# In[257]:
 
 
 ## Replace '' with '0'
@@ -38,22 +20,11 @@ for i in range(len(input_raw)):
         input_raw[i]='0'
 
 
-# In[258]:
-
-
 ## Change elements datatype to int
 input_raw2 = list(map(int, input_raw))
 input_raw2[0:20]
 
-
-# In[184]:
-
-
 ##breaks_ind = [i for i in range(len(input_raw)) if input_raw[i] == '0']
-
-
-# In[145]:
-
 
 ## Another way to find ind of breaks
 ## breaks_ind=[]
@@ -61,11 +32,7 @@ input_raw2[0:20]
     ## if input_raw[i]=='':
         ## breaks_ind.append(i)   
 
-
-# In[275]:
-
-
-## Assign group, one group represent one elf
+## Assign elf
 elf = []
 n = 1
 for i in range(len(input_raw2)):
@@ -75,66 +42,26 @@ for i in range(len(input_raw2)):
         elf.append('Break')
         n = n +1       
 
-
-# In[260]:
-
-
 elf[0:20]
-print(len(input_raw2),len(group))
+print(len(input_raw2),len(elf))
 
-
-# In[336]:
-
-
+## Merge calories and elf together
 input_raw_df = pd.DataFrame(data = input_raw2, columns = ['calories'])
 elf_df = pd.DataFrame(data = elf, columns = ['elf'])
 df = pd.concat([input_raw_df,elf_df],axis=1)
 
-
-# In[337]:
-
-
+## Calculate total calories of each elf
 df['tot_calories'] = df.groupby(['elf'])['calories'].transform('sum')
 
-
-# In[357]:
-
-
+## Calculate the max total calories 
 df_max = df['tot_calories'].max()
 
-
-# In[358]:
-
-
-print(df_max)
-
-
-# In[373]:
-
-
+## Find which elf carries the most calories
 max_elf = df['elf'][df['tot_calories']==74394].unique()
 
-
-# In[374]:
-
-
-print(max_elf)
-
-
-# In[375]:
-
-
+## This elf carries how many calories respectively 
 max_elf_calories = df['calories'][df['tot_calories']==74394].unique()
 
-
-# In[376]:
-
-
-print(max_elf_calories)
-
-
-# In[377]:
-
-
+## Print results
 print('The', +max_elf, 'carries the most calories. \n This elf carries', +max_elf_calories, '.\n', 'Total calories is', +df_max)
 
